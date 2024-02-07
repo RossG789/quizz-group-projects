faee5375ab2400b1f312510563f193dac08b9194
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-02-07 10:38:53 +0000

    committing code for modal for you both to have a look for suggestions

--- client/main.js
+++ client/main.js
@@ -7,3 +7,15 @@ async function fetchQuiz() {
   console.log(result);
   return result;
 }
+
+const openModal = document.getElementById("open");
+const closeModal = document.getElementById("close");
+const modalContainer = document.getElementById("modal_container");
+
+openModal.addEventListener("click", () => {
+  modalContainer.classList.add("show");
+});
+
+closeModal.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
