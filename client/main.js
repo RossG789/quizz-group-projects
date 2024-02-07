341282d28e743c98a0bfe1109380fd3e782cd5df
RossG789 <gossrray@gmail.com>
2024-02-07 14:25:18 +0000

    Workable version of our display

--- client/main.js
+++ client/main.js
@@ -1,11 +1,11 @@
 const startQuiz = document.getElementById("start-quiz");
 const quizQuestDiv = document.getElementById("quiz-quest");
 const quizAnswerDiv = document.getElementById("quiz-answers");
-const quizItemForReal = document.getElementById("quizItem");
+const quizItemDiv = document.getElementById("quiz-item-div");
+let result;
 let currentIndex = 0;
 //defult global url
 let baseUrl = "http://localhost:1212";
-let resultArray;
 //fetch request to route on server.js
 async function fetchQuiz() {
   const quiz = await fetch(`${baseUrl}/quiz`);
@@ -14,51 +14,29 @@ async function fetchQuiz() {
   return result;
 }
 
-init();
+startQuiz.addEventListener("click", init);
 
 async function init() {
   resultArray = await fetchQuiz();
   createMain(resultArray[currentIndex]);
 }
-// when they click on an answer,
-
-// startQuiz.addEventListener("click", displayQuiz);
-
-// async function displayQuiz() {
-//   let quizObjects = await fetchQuiz();
-
-//   console.log(quizObjects);
-
-//   quizObjects.forEach((object) => {
-//     let quizItemContainer = document.createElement("div");
-//     let quizQuestion = document.createElement("p");
-//     quizQuestion.innerHTML = object.question;
-
-//     quizItemContainer.appendChild(quizQuestion);
-//     object.answers.forEach((answer) => {
-//       let quizAnswerTag = document.createElement("p");
-//       quizAnswerTag.innerHTML = answer;
-//       quizItemContainer.appendChild(quizAnswerTag);
-//     });
-
-//     quizItem.appendChild(quizItemContainer);
-//   });
-// }
 
 // intial call
 
 function createMain(quizItem) {
-  console.log(quizItem);
+  // console.log(quizItem);
   let quizItemContainer = document.createElement("div");
   let quizQuestion = document.createElement("p");
   quizQuestion.innerHTML = quizItem.question;
 
   quizItemContainer.appendChild(quizQuestion);
+  quizItemDiv.appendChild(quizQuestion);
   quizItem["answers"].forEach((answer) => {
-    console.log(answer);
+    // console.log(answer);
+
     let quizAnswerTag = document.createElement("p");
     quizAnswerTag.innerHTML = answer;
-    quizItemForReal.appendChild(quizAnswerTag);
+    quizItemDiv.appendChild(quizAnswerTag);
 
     quizAnswerTag.addEventListener("click", () => {
       // check if correct function
@@ -67,5 +45,5 @@ function createMain(quizItem) {
     });
   });
 
-  quizItemForReal.appendChild(quizQuestion);
+  // quizItemDiv.appendChild(quizQuestion);
 }
